2c1b0da29deb40217c1812b64256b422d6b4c2c0
ZheJia47 <53133006+ZheJia47@users.noreply.github.com>
2020-09-10 19:48:45 +0800

    create local repository

---
+++ youtube_downlaoder.py
@@ -0,0 +1,21 @@
+# 在python資料夾底下執行“Install Certificates.command”解決驗證問題
+# cd "/Applications/Python 3.8/" 
+# sudo "./Install Certificates.command"
+
+from pytube import YouTube
+import os
+from pprint import pprint
+
+while True:
+    download_link = input('輸入下載網址: ')
+    yt = YouTube(download_link)
+
+    # save path
+    save_path = os.path.expanduser('~') + '/Downloads/music' 
+
+    pprint(yt.streams.all())
+    index = int(input('index = '))
+    yt.streams.all()[index].download(save_path)
+
+    if download_link == 'q': break
+
