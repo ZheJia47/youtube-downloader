6888bbd7e8805ff3aa40ad32e7b16c5e6e7e0454
ZheJia47 <53133006+ZheJia47@users.noreply.github.com>
2020-09-25 16:53:46 +0800

    0925

--- youtube_downlaoder.py
+++ youtube_downlaoder.py
@@ -5,19 +5,26 @@
 from pytube import YouTube
 import os
 from pprint import pprint
+from selenium import webdriver
+from time import sleep
 
+browser=webdriver.Chrome()
 # path to save file
 save_path = os.path.expanduser('~') + '/Downloads/music' 
 
 while True:
     download_link = input('輸入下載網址: ')
-    yt = YouTube(download_link)
 
+    # todo: use 'youtube + to' website to download
+    # browser.get(download_link)
+
+
+
+    # use pytube ######################
+    yt = YouTube(download_link)
     pprint(yt.streams.all())
     index = int(input('index = '))
     yt.streams.all()[index].download(save_path)
 
     if download_link == 'q': break
-
-# todo: use 'youtube + to' website to download
 
